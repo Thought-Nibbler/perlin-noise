2ef2affa95600ba44178a4e04438fcc637ea4788
Thought-Nibbler <Thought-Nibbler@example.com>
2024-10-14 08:47:08 +0900

    不要なシェーダのハードコーディングを削除

--- src/triangle.py
+++ src/triangle.py
@@ -62,32 +62,6 @@ class Shader:
     def unuse(self):
         glUseProgram(0)
 
-vert = """
-#version 450
-
-layout (location = 0) in vec2 vertex;
-layout (location = 1) in vec3 color;
-
-layout (location = 0) out vec3 outColor;
-
-void main() {
-    gl_Position = vec4(vertex, 0, 1);
-    outColor = color;
-}
-"""
-
-frag = """
-#version 450
-
-layout (location = 0) in vec3 color;
-
-layout (location = 0) out vec4 outColor;
-
-void main() {
-    outColor = vec4(color, 1);
-}
-"""
-
 
 def main() -> None:
     """
